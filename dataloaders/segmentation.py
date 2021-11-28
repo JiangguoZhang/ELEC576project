620ff0bf5230be4e13ef8cf40cce7cd897aa44df
Machine-Trainer <starkzeng2333@gmail.com>
2021-11-28 01:13:19 -0600

    IOU function is implemented in the segmentation.py file

--- dataloaders/segmentation.py
+++ dataloaders/segmentation.py
@@ -1,12 +1,27 @@
 from PairedNeurons import PairedNeurons
 from matplotlib import pyplot as plt
 import os
+import numpy as np
 import cv2
+SMOOTH = 1e-6
+def iou_numpy(outputs: np.array, labels: np.array):
+    # outputs = outputs.squeeze(2)
+    
+    intersection = (outputs & labels).sum((0, 1))
+    union = (outputs | labels).sum((0, 1))
+    
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+    
+    thresholded = np.ceil(np.clip(20 * (iou - 0.5), 0, 10)) / 10
+    
+    return thresholded.mean()  # Or thresholded.mean()
+
 img_dir = "/Users/mac/Desktop/Rice-COMP576/sartorius-cell-instance-segmentation/train"
 csv_dir = "/Users/mac/Desktop/Rice-COMP576/sartorius-cell-instance-segmentation/train.csv"
 
 pn = PairedNeurons(img_dir, csv_dir, 256, is_train=False)
-for i in range(6):
+sum=0
+for i in range(len(pn)):
     x, y, l = pn.__getitem__(i)
 
     # fig, axs = plt.subplots(1, 2, figsize=(16, 8))
@@ -21,7 +36,11 @@ for i in range(6):
     # fig.tight_layout()
     # plt.savefig(os.path.join("./save", l))
     # plt.close()
-    plt.subplot(2, 3, i + 1)
-    plt.title(l)
-    plt.imshow(y, 'gray')
-plt.show()
+    # plt.subplot(2, 3, i + 1)
+    thresholdValue=0.07
+    ret, th1 = cv2.threshold(x, thresholdValue, 0.1, cv2.THRESH_BINARY)
+    # print(iou_numpy((x*255).astype(int),(th1*255).astype(int)))
+    sum+=iou_numpy((x*255).astype(int),(th1*255).astype(int))
+    # plt.title(l)
+    # plt.imshow(x*255, 'gray')
+print(sum/len(pn))
--- dataloaders/testCv2.py
+++ dataloaders/testCv2.py
@@ -1,19 +1,26 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
-
+import numpy as np
+SMOOTH = 1e-6
+def iou_numpy(outputs: np.array, labels: np.array):
+    # outputs = outputs.squeeze(2)
+    
+    intersection = (outputs & labels).sum((0, 1))
+    union = (outputs | labels).sum((0, 1))
+    
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+    
+    thresholded = np.ceil(np.clip(20 * (iou - 0.5), 0, 10)) / 10
+    
+    return thresholded.mean()  # Or thresholded.mean()
 # 1.读取图像
-img = cv.imread('/Users/mac/Desktop/Rice-COMP576/sartorius-cell-instance-segmentation/train/0030fd0e6378/0030fd0e6378.png', 0)
+img = cv.imread('/Users/mac/Desktop/Rice-COMP576/sartorius-cell-instance-segmentation/train/0030fd0e6378/0030fd0e6378.png',0)
 # 2. 阈值分割
 thresholdValue=135
 ret, th1 = cv.threshold(img, thresholdValue, 255, cv.THRESH_BINARY)
 ret,th2 = cv.threshold(img,200,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 ret, th4 = cv.threshold(img, thresholdValue, 255, cv.THRESH_TOZERO)
 
-# noise removal
-kernel = np.ones((2,2),np.uint8)
-#opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
-closing = cv2.morphologyEx(thresh,cv2.MORPH_CLOSE,kernel, iterations = 2)
-sure_bg = cv2.dilate(closing,kernel,iterations=3)
 
 # 3. 图像显示
 titles = ['original', 'th1', 'th2', 'th3', 'th4', 'th5']
